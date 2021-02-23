60a1a0d3a655c60d04a7c0ed5763d9041bcf8892
Juan Alberto <jguzman.pearbit@gmail.com>
2021-02-23 13:58:03 -0600

    Aviso de Privacidad Modificado

--- resources/views/aviso.blade.php
+++ resources/views/aviso.blade.php
@@ -21,52 +21,55 @@
 proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de
 los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser
  identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo
-  esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos
-   revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios. y
-como responsable del tratamiento de sus datos personales, hace de su conocimiento que la información de nuestros
+  este Aviso de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos
+   revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios. Somos responsable del
+   tratamiento de sus datos personales, hace de su conocimiento que la información de nuestros
 clientes es tratada de forma estrictamente confidencial por lo que al proporcionar sus datos personales, tales como:</p>
 <ul>
 <li> 1. Nombre Completo.</li>
 <li> 2. Dirección.</li>
 <li> 3. CURP.</li>
 <li> 3. RFC.</li>
-<li> 4. Teléfonos de Hogar, Oficina y móviles</li>
+<li> 4. Teléfono</li>
 <li> 5. Correo Electrónico.</li>
 </ul><br>
-<p>Estos serán utilizados única y exclusivamente para los siguientes fines:</p>
-1. Información y Prestación de Servicios.
-2. Actualización de la Base de Datos.
-3. Cualquier finalidad análoga o compatible con las anteriores.
-En el caso de Datos sensibles, tales como:
-1. Datos Financieros (Ingresos, Estados de Cuenta, y demás relacionados)
-2. Datos Patrimoniales (Bienes Materiales, Inmuebles, y demás relacionados)
-3. Datos Personales (Cónyuge, Estado Civil, Nacionalidad, Educación, Hijos, y demás relacionados).
-4. Referencias familiares y no familiares (Nombre, Dirección, Teléfono, relación, etc.).
-Estos serán utilizados única y exclusivamente para los siguientes fines:
-1. Investigación y/u Obtención de Créditos ante las Instituciones Financieras.
-2. Cualquier finalidad análoga o compatible con la anterior.
-3. Información y Prestación de Servicios
-Para prevenir el acceso no autorizado a sus datos personales y con el fin de asegurar que la información sea utilizada para
+
+<h4>Estos serán utilizados única y exclusivamente para los siguientes fines:</h4>
+<ul>
+<li>1. Información y Prestación de Servicios.</li>
+<li>2. Actualización de la Base de Datos.</li>
+<li>3. Generar reporte del estado en que se encuentra la persona psicológicamente.</li><br>
+</ul>
+<h4>En el caso de Datos sensibles, tales como:</h4>
+<ul>
+<li>1. Datos Personales (Nombre Completo, RFC, CURP, Edad, Teléfono, y demás relacionados).</li>
+</ul><br>
+
+<h4>Estos serán utilizados única y exclusivamente para los siguientes fines:</h4>
+<ul>
+<li>1. Información y Prestación de Servicios</li>
+</ul><br>
+
+<p>Para prevenir el acceso no autorizado a sus datos personales y con el fin de asegurar que la información sea utilizada para
 los fines establecidos en este aviso de privacidad, hemos establecido diversos procedimientos con la finalidad de evitar el
-uso o divulgación no autorizados de sus datos, permitiéndonos tratarlos debidamente.
-Así mismo, le informamos que sus datos personales pueden ser Transmitidos para ser tratados por personas distintas a esta
-empresa.
+uso o divulgación no autorizados de sus datos, permitiéndonos tratarlos debidamente.</p>
+<p>Así mismo, le informamos que sus datos personales pueden ser Transmitidos para ser tratados por personas distintas a esta
+empresa.</p>
+<p>
 Todos sus datos personales son tratados de acuerdo a la legislación aplicable y vigente en el país, por ello le informamos
 que usted tiene en todo momento los derechos (ARCO) de acceder, rectificar, cancelar u oponerse al tratamiento que le
 damos a sus datos personales; derecho que podrá hacer valer a través del Área de Privacidad encargada de la seguridad
-de datos personales en el Teléfono (55) 5611- 94 -6 2, o por medio de su correo electrónico:
-admin@fervel.com.mx
-A través de estos canales usted podrá actualizar sus datos y especificar el medio por el cual desea recibir información, ya
-que en caso de no contar con esta especificación de su parte, Fervel Asesoría Integral S.C. establecerá libremente el
-canal que considere pertinente para enviarle información.
-Este aviso de privacidad podrá ser modificado por Fervel Asesoría Integral S.C., dichas modificaciones serán
-oportunamente informadas a través de correo electrónico, teléfono, o cualquier otro medio de comunicación que
-Fervel Asesoría Integral S.C. determine para tal efecto.
-ATENTAMENTE
+de datos personales en el Teléfono (+52) 5542-77-0668, o por medio de su correo electrónico:
+empresacontigo035@gmail.com</p>
 
-    </div>
-    <small><p class="text-center text-primary"><b>Empresa Contigo</b> Se reserva el derecho de cambiar los términos de la presente Política de Privacidad
-        en cualquier momento.</p></small>
+<p>A través de estos canales usted podrá actualizar sus datos y especificar el medio por el cual desea recibir información.</p>
+<p>Este aviso de privacidad podrá ser modificado por Empresa Contigo S.A de C.V., dichas modificaciones serán
+oportunamente informadas a través de correo electrónico, teléfono, o cualquier otro medio de comunicación que
+Empresa Contigo S.A de C.V. determine para tal efecto.</p><br>
+
+<h4 class="text-center">ATENTAMENTE</h4><br>
+<center><img src="{{ asset('assets/general/img/logo/loder.jpg')}}" alt=""></center>
+<h3 class="text-center"><b><u>Empresa Contigo S.A de C.V</u></b></h3>
 
 
 @endsection
--- resources/views/templateGral.blade.php
+++ resources/views/templateGral.blade.php
@@ -107,7 +107,7 @@
                                 <div class="single-footer-caption mb-30">
                                     <!-- logo -->
                                     <div class="footer-logo">
-                                        <a href="index.html"><img src="{{ asset('assets/general/img/logo/footer.png')}}" alt=""></a>
+                                        <a href="index.html"><img src="{{ asset('assets/general/img/logo/logo2_footer.png')}}" alt=""></a>
                                     </div>
                                 </div>
                             </div>
