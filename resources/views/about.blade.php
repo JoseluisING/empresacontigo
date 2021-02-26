775091adc5fa7f90efa2f4f40876c3b04a793891
Alan <alan2309988@gmail.com>
2021-02-25 19:05:09 -0600

    Agregar info index y about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-xl-12">
                 <div class="hero-cap text-center pt-100">
-                    <h2>About us</h2>
+                    <h2>Empresa Contigo</h2>
                 </div>
             </div>
         </div>
@@ -20,175 +20,41 @@
         <div class="row">
             <div class="col-lg-4">
                 <div class="about-tittle">
-                    <h2>About Us</h2>
+                    <h2>Acerca de Nosotros</h2>
                 </div>
             </div>
             <div class="col-lg-7">
                 <div class="about-details mb-50">
-                    <p class="pear1">We believe that every project existing in digital world is a result of an idea and every idea has a cause. </p>
-                    <p class="pear2">Amet, consectetur adipisicing elit, sed dosmod tempor didunt ut labore et dolor dfagna iqus ad minim veniam quis. sectetur adipisicing eld do eiusmod tempor didunt ut labore et dolore magna aliqus ad minim.</p>
+                    <p class="pear1">Empresa Contigo nace en Tecámac, Estado de México, el 27 de enero del 2021, conformado por un grupo de emprendedores, llenos de carácter, entusiastas y comprometidos con su país. observaron la oportunidad para desarrollar las habilidades que de ellos emanan, durante mucho tiempo, un análisis exacto y preciso sobre su entorno, llevo a Lizeth Parra, Juan Guzmán, Alan Emanuel Ugalde y Badillo José Luis, a generar un fuerte impacto social, cuyo principal objetivo es aterrizar sobre el sector tecnológico. Preparados para cubrir con el abastecimiento de sistemas informáticos, para lograr un derroche de soluciones, que cambian el rumbo y las decisiones de quienes consumen estos servicios día con día. </p>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="about-tittle">
-                    <h2>Mission</h2>
+                    <h2>Visión</h2>
                 </div>
             </div>
             <div class="col-lg-7">
                 <div class="about-details">
-                    <p class="pear1">We believe that every project existing in digital world is a result of an idea and every idea has a cause. </p>
-                    <p class="pear2">Amet, consectetur adipisicing elit, sed dosmod tempor didunt ut labore et dolor dfagna iqus ad minim veniam quis. sectetur adipisicing eld do eiusmod tempor didunt ut labore et dolore magna aliqus ad minim.</p>
-                    <p class="pear2">Amet, consectetur adipisicing elit, sed dosmod tempor didunt ut labore et dolor dfagna iqus ad minim veniam quis. sectetur adipisicing eld do eiusmod tempor didunt ut labore et dolore magna aliqus ad minim.</p>
+                    <p class="pear1">Ser el motor de las pequeñas,medianas y grandes empresas para que puedan cumplir con la NOM 035,para que aumenten su eficacia y productividad.  </p>
+                 </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="about-tittle">
+                    <h2>Misión</h2>
                 </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="about-details">
+                    <p class="pear1">Nuestra misión es desarrollar e innovar sistemas
+                        informáticos de excelencia a organizaciones que quieran cumplir con la NOM 035, nosotros dedicamos el 100% a la satisfacción de cada uno
+                        de nuestros clientes.</p>
+                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- About Details End -->
-<!--All startups Start -->
-<div class="all-starups-area fix">
-    <!-- left Contents -->
-    <div class="starups">
-        <div class="starups-details">
-            <span>Features That You Will Get</span>
-            <h3>All startups need to make<br>their business work</h3>
-            <!-- Details -->
-            <div class="starups-list">
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Having a home based business is a wonderful asset to your life. The problem still stands,</p>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Promotional Advertising Specialty You Ve Waited Long Enough</p>
-                    </li>
-                </ul>
-                <ul>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30px" height="30px">
-                        <path fill-rule="evenodd"  fill="rgb(4, 219, 236)" d="M15.000,0.000 C23.284,0.000 30.000,6.716 30.000,15.000 C30.000,23.284 23.284,30.000 15.000,30.000 C6.716,30.000 -0.000,23.284 -0.000,15.000 C-0.000,6.716 6.716,0.000 15.000,0.000 Z"/>
-                        <text kerning="auto" font-family="themify" fill="rgb(255, 255, 255)" font-size="14px" x="9px" y="22.0320000000002px">&#58956;</text>
-                        </svg>
-                    </li>
-                    <li>
-                        <p>Direct Mail Advertising How I Made 47 325 In 30 Days  By Mailing 2 200 Letters</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!--Right Contents  -->
-    <div class="starups-img"></div>
-</div>
-<!--All startups End -->
-<!-- Testimonial Start -->
-<div class="testimonial-area testimonial-padding">
-    <div class="container ">
-        <div class="row d-flex justify-content-center">
-            <div class="col-xl-11 col-lg-11 col-md-9">
-                <div class="h1-testimonial-active">
-                    <!-- Single Testimonial -->
-                    <div class="single-testimonial text-center">
-                            <!-- Testimonial Content -->
-                        <div class="testimonial-caption ">
-                            <div class="testimonial-top-cap">
-                                <img src="assets/img/icon/testimonial.png" alt="">
-                                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis por incididunt ut labore et dolore mas. </p>
-                            </div>
-                            <!-- founder -->
-                            <div class="testimonial-founder d-flex align-items-center justify-content-center">
-                                <div class="founder-img">
-                                    <img src="assets/img/testmonial/Homepage_testi.png" alt="">
-                                </div>
-                                <div class="founder-text">
-                                    <span>Oliva jems</span>
-                                    <p>UIX designer</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Single Testimonial -->
-                    <div class="single-testimonial text-center">
-                            <!-- Testimonial Content -->
-                        <div class="testimonial-caption ">
-                            <div class="testimonial-top-cap">
-                                <img src="assets/img/icon/testimonial.png" alt="">
-                                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis por incididunt ut labore et dolore mas. </p>
-                            </div>
-                            <!-- founder -->
-                            <div class="testimonial-founder d-flex align-items-center justify-content-center">
-                                <div class="founder-img">
-                                    <img src="assets/img/testmonial/Homepage_testi.png" alt="">
-                                </div>
-                                <div class="founder-text">
-                                    <span>Oliva jems</span>
-                                    <p>UIX designer</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Testimonial End -->
-<!-- Visit Our Tailor Start -->
-<div class="visit-tailor-area fix visit-pt-200">
-    <!--Right Contents  -->
-    <div class="tailor-offers"></div>
-    <!-- left Contents -->
-    <div class="tailor-details">
-        <span>All of our Skills In The Fild</span>
-        <h3>Differentiate Yourself And <br>Attractive Attention</h3>
-        <p>Tempor incididunt ut labore et cvhjfhore magna aliqua. Ut<br> enim ad minim veniam, quis.</p>
-            <!-- skill Start -->
-        <div class="skill-ara">
-            <div class="skill">
-                <div class="single-skill">
-                    <div class="progress-count">
-                        <p>User Interface</p><span>75%</span>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar wow slideInLeft" data-wow-duration="1s" data-wow-delay=".5s" aria-valuenow="25" role="progressbar" style="width: 75%"  aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <div class="single-skill">
-                    <div class="progress-count">
-                        <p>Digital Marketing</p><span>89%</span>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar wow slideInLeft" data-wow-duration="1s" data-wow-delay=".7s" role="progressbar" style="width: 89%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <div class="single-skill">
-                    <div class="progress-count">
-                        <p>User Interface</p><span>95%</span>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar wow slideInLeft" data-wow-duration="1s" data-wow-delay=".6s" role="progressbar" style="width: 95%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- skill End -->
-    </div>
 
-</div>
-<!-- Visit Our Tailor End -->
 
 <!-- Want To Work Start -->
 <div class="wantToWork-area wantToWork-area2 w-padding2">
@@ -196,11 +62,11 @@
         <div class="row align-items-center justify-content-between">
             <div class="col-xl-8 col-lg-8 col-md-8">
                 <div class="wantToWork-caption wantToWork-caption2">
-                    <h2>Send us a message if you have an issue with your order.</h2>
+                    <h2>Envianos un mensaje.</h2>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4">
-                <a href="#" class="btn btn-black f-right">contact us now</a>
+                <a href="#" class="btn btn-black f-right">Contactanos</a>
             </div>
         </div>
     </div>
