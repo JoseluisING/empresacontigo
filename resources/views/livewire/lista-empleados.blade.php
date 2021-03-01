ce2e000e3be553ceefc4fc3ccf166e0ea0ff135b
Juan Alberto <jguzman.pearbit@gmail.com>
2021-03-01 17:15:38 -0600

    Perdon :v

--- resources/views/livewire/lista-empleados.blade.php
+++ resources/views/livewire/lista-empleados.blade.php
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-
-            {{ __('Dashboard') }} / Bienvenid@: {{ Auth::user()->name .' '. Auth::user()->ap_p.' '. Auth::user()->ap_m}}
-            @if(Auth::user()->id_rol==1)
-            <div class="inline-block mr-2 mt-2">
-                <button type="button" class="focus:outline-none text-blue-600 text-sm py-2.5 px-5 rounded-md hover:bg-blue-100">Admin</button>
-             </div>
-            @else
-            <div class="inline-block mr-2 mt-2">
-                <button type="button" class="focus:outline-none text-green-600 text-sm py-2.5 px-5 rounded-md hover:bg-green-100">Empleado</button>
-             </div>
-            @endif
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
-=======
 
 
 <div class="py-12">
@@ -118,4 +93,3 @@
         </div>
     </div>
 </div>
->>>>>>> b62e9319a930e0f31990f64e1a6e074ee8f25560
